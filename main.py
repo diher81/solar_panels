a747a6375c7c96c14ce9b29beacbd6d36f970403
Dirk Hermans <hermans.dirk@hotmail.com>
2026-06-10 16:09:58 +0200

    use env file

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+import smtplib
+from email.message import EmailMessage
+
 import requests
 from statistics import median
+from dotenv import load_dotenv
 
-SITE_ID = os.environ["SE_SITE_ID"]
-API_KEY = os.environ["SE_API_KEY"]
+load_dotenv()
+
+SITE_ID = os.getenv("SE_SITE_ID")
+API_KEY = os.getenv("SE_API_KEY")
+
+print("SITE_ID =", SITE_ID)
+print("API_KEY present =", API_KEY is not None)
 
 SMTP_HOST = os.environ["SMTP_HOST"]
 SMTP_PORT = int(os.environ.get("SMTP_PORT", "587"))
@@ -12,6 +21,18 @@ SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
 
 MAIL_FROM = os.environ["MAIL_FROM"]
 MAIL_TO = os.environ["MAIL_TO"]
+
+# def send_mail(subject, body):
+#     msg = EmailMessage()
+#     msg["Subject"] = subject
+#     msg["From"] = MAIL_FROM
+#     msg["To"] = MAIL_TO
+#     msg.set_content(body)
+#
+#     with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as smtp:
+#         smtp.starttls()
+#         smtp.login(SMTP_USER, SMTP_PASSWORD)
+#         smtp.send_message(msg)
 
 def get_optimizer_data():
     url = (
